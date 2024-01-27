06b955b417bba85c1f64c9e1e8cb133b420c35ef
jorotyn <jiecumming@gmail.com>
2024-01-28 10:49:08 +1100

    Improved AI drowning state

--- Pool Rules/Assets/Scripts/AI/Actions/AIActionDrowning.cs
+++ Pool Rules/Assets/Scripts/AI/Actions/AIActionDrowning.cs
@@ -1,19 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIActionDrowning : AIAction
 {
     [SerializeField] private ChildRuleStates childRuleStates;
+    [SerializeField] private NavMeshAgent childNavMeshAgent;
+    [SerializeField] private float drowningBaseOffset = 0.15f;
+
+    private float originalBaseOffset;
 
     private void Awake()
     {
+        if (childNavMeshAgent == null) childNavMeshAgent = GetComponentInParent<NavMeshAgent>();
+        if (childNavMeshAgent == null) Debug.LogWarning("childNavMeshAgent is null");
         if (childRuleStates == null) childRuleStates = GetComponentInParent<ChildRuleStates>();
         if (childRuleStates == null) Debug.LogWarning("childRuleStates is null");
     }
 
+    private void Start()
+    {
+        originalBaseOffset = childNavMeshAgent.baseOffset;
+    }
+
+    public override void OnEnterState()
+    {
+        base.OnEnterState();
+        childNavMeshAgent.baseOffset = drowningBaseOffset;
+    }
+
+    public override void OnExitState()
+    {
+        base.OnExitState();
+        childNavMeshAgent.baseOffset = originalBaseOffset;
+    }
+
     public override void PerformAction()
     {
-        childRuleStates.IsDrowning = true;
+        if (childNavMeshAgent.isActiveAndEnabled)
+        {
+            childRuleStates.IsDrowning = true;
+        }
     }
 }
