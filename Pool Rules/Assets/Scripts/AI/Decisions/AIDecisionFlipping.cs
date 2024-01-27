ba44dc8411478cbd29caf95eda6e8af8780a143c
jorotyn <jiecumming@gmail.com>
2024-01-27 21:47:37 +1100

    Implemented AI state for flipping

---
+++ Pool Rules/Assets/Scripts/AI/Decisions/AIDecisionFlipping.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDecisionFlipping : AIDecision
+{
+    [SerializeField] private ChildRuleStates childRuleStates;
+    [SerializeField] private int totalChance = 50;
+    [SerializeField] private int odds = 1;
+
+    private void Awake()
+    {
+        if (childRuleStates == null) Debug.LogWarning("ChildRuleStates is null");
+    }
+
+    public override bool Decide()
+    {
+        if (childRuleStates.IsSwimming) return false;
+        else return EvaluateOdds();
+    }
+
+    private bool EvaluateOdds()
+    {
+        int dice = Maths.RollADice(totalChance);
+        bool result = (dice <= odds);
+        return result;
+    }
+}
