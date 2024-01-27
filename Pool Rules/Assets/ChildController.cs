374b7c1bd9bdffdd0f96856d524412f83d1d235e
Andrew Anastasi <drewco.australia@gmail.com>
2024-01-27 11:56:07 +1100

    Basic movement and yeeting implemented

---
+++ Pool Rules/Assets/ChildController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildController : MonoBehaviour
+{
+    public Material MatDefault;
+    public Material MatSelected;
+    public Material MatNaughty;
+    private MeshRenderer _rend;
+    private Material MatCurrent;
+    private bool isSelected = false;
+
+    private void Awake()
+    {
+        _rend = GetComponent<MeshRenderer>();
+        MatCurrent = MatDefault;
+        _rend.material = MatCurrent;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            MatCurrent = MatNaughty;
+            _rend.material = MatCurrent;
+        }
+
+        if (isSelected)
+        {
+            _rend.material = MatSelected;
+        }
+        else
+        {
+            _rend.material = MatCurrent;
+        }
+    }
+
+    public void SetSelected(bool selected = true)
+    {
+        isSelected = selected;
+    }
+}
---
+++ Pool Rules/Assets/PlayerController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float pickupDist = 1f;
+    public float throwPower = 1f;
+    public float throwAngle = 30f; // positive is towards floor, negative towards sky
+    public float throwSpinMin = 30f;
+    public float throwSpinMax = 90f;
+    private Vector3 velocity = Vector2.zero;
+    public Transform raycastOrigin;
+    public Transform pickupPosition;
+    private GameObject raycastHitObj = null;
+    private bool handsEmpty = true;
+    private GameObject heldObj = null;
+
+    void Start()
+    {
+        
+    }
+
+    void PerformRaycast()
+    {
+        /* 
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+        */
+
+        RaycastHit hit;
+        if (handsEmpty && Physics.Raycast(raycastOrigin.position, transform.TransformDirection(Vector3.forward), out hit, pickupDist/*, layerMask*/))
+        {
+            Debug.DrawRay(raycastOrigin.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if (raycastHitObj != null && raycastHitObj != hit.transform.gameObject)
+            {
+                raycastHitObj.GetComponent<ChildController>().SetSelected(false);
+            }
+            raycastHitObj = hit.transform.gameObject;
+            raycastHitObj.GetComponent<ChildController>().SetSelected(true);
+        }
+        else
+        {
+            Debug.DrawRay(raycastOrigin.position, transform.TransformDirection(Vector3.forward) * pickupDist, Color.white);
+            if (raycastHitObj != null)
+            {
+                raycastHitObj.GetComponent<ChildController>().SetSelected(false);
+                raycastHitObj = null;
+            }
+        }
+    }
+
+    void Update()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+        transform.position += velocity * Time.deltaTime;
+        transform.LookAt(transform.position + velocity);
+
+        if (handsEmpty) PerformRaycast();
+
+        if (Input.GetKeyDown(KeyCode.Space)) Debug.Log("Space pressed");
+
+        if (handsEmpty && raycastHitObj != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            handsEmpty = false;
+            heldObj = raycastHitObj;
+            heldObj.GetComponent<ChildController>().SetSelected(false);
+            heldObj.transform.parent = transform;
+            heldObj.transform.position = pickupPosition.position;
+            heldObj.GetComponent<Rigidbody>().useGravity = false;
+            raycastHitObj = null;
+        }
+        else if (!handsEmpty && heldObj != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            handsEmpty = true;
+            heldObj.transform.parent = null;
+            heldObj.GetComponent<Rigidbody>().useGravity = true;
+
+            Vector3 throwForce = throwPower * (Quaternion.AngleAxis(throwAngle, transform.TransformDirection(Vector3.right)) * transform.TransformDirection(Vector3.forward));
+            Debug.Log(throwForce);
+            heldObj.GetComponent<Rigidbody>().AddForce(throwForce,ForceMode.Impulse);
+            float spin = Random.Range(throwSpinMin, throwSpinMax);
+            heldObj.GetComponent<Rigidbody>().AddTorque(transform.right * spin, ForceMode.Impulse);
+            heldObj = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(raycastOrigin.position, transform.TransformDirection(Vector3.forward) * pickupDist, Color.white);
+    }
+}
