b0ec44d48b3a79c6971cd9f1471fc4760f77441b
jorotyn <jiecumming@gmail.com>
2024-01-28 11:49:40 +1100

    Tweaked the set of scheduled rules

--- Pool Rules/Assets/Scripts/Managers/RuleScheduler.cs
+++ Pool Rules/Assets/Scripts/Managers/RuleScheduler.cs
@@ -6,8 +6,25 @@ using UnityEngine;
 public class ScheduledRuleChange
 {
     public string ruleName;
-    public float changeTime;
-    public bool newState;
+    public float startTime;
+    public float timer;
+
+    public ScheduledRuleChange(string ruleName, float startTime, float timer)
+    {
+        this.ruleName = ruleName;
+        this.startTime = startTime;
+        this.timer = timer;
+    }
+
+    public bool ShouldStart()
+    {
+        return Time.time >= startTime;
+    }
+
+    public bool IsTimeElapsed()
+    {
+        return Time.time >= startTime + timer;
+    }
 }
 
 public class RuleScheduler : MonoBehaviour
@@ -15,26 +32,26 @@ public class RuleScheduler : MonoBehaviour
     public List<ScheduledRuleChange> scheduledChanges;
 
     private RuleManager _ruleManager;
-    private float _gameTime;
 
     void Start()
     {
         _ruleManager = RuleManager.Instance;
-        _gameTime = 0f;
     }
 
     void Update()
     {
-        _gameTime += Time.deltaTime;
-
-        for (int i = 0; i < scheduledChanges.Count; i++)
+        for (int i = scheduledChanges.Count - 1; i >= 0; i--)
         {
             ScheduledRuleChange change = scheduledChanges[i];
-            if (_gameTime >= change.changeTime)
+            if (change.ShouldStart())
             {
-                _ruleManager.SetRule(change.ruleName, change.newState);
+                _ruleManager.SetRule(change.ruleName, true);
+            }
+
+            if (change.IsTimeElapsed())
+            {
+                _ruleManager.SetRule(change.ruleName, false);
                 scheduledChanges.RemoveAt(i);
-                i--;
             }
         }
     }
