b4402c06630809d00096d3d8d333191f491c4d9d
jorotyn <jiecumming@gmail.com>
2024-01-28 14:49:02 +1100

    Imported new ambient SFX track

--- Pool Rules/Assets/Scripts/Utilities/SceneController.cs
+++ Pool Rules/Assets/Scripts/Utilities/SceneController.cs
@@ -3,9 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartScene();
+        }
+    }
+
     public void RestartScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 }
