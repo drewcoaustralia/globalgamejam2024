85b8a38e12d01e92bbefc14bc370ea89584bd5b9
Andrew Anastasi <drewco.australia@gmail.com>
2024-03-11 19:52:30 +1100

    Changed Audio Manager to handle new music across scenes

--- Pool Rules/Assets/Scripts/AudioManager.cs
+++ Pool Rules/Assets/Scripts/AudioManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
 
+    public AudioClip menuMusic;
     public AudioClip bgm;
     public AudioClip ambient;
     private List<AudioSource> _sources;
@@ -15,16 +17,37 @@ public class AudioManager : MonoBehaviour
     {
         if (Instance != null && Instance != this) Destroy(this);
         else Instance = this;
+        DontDestroyOnLoad(gameObject);
+        _sources = GetComponents<AudioSource>().ToList();
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("scene loaded " + scene.name);
+        _sources[0].Stop();
+        if (scene.name == "MainMenu")
+        {
+            _sources[0].loop = true;
+            _sources[0].clip = menuMusic;
+            _sources[0].volume = 0.7f;
+            _sources[0].Play();
+        }
+        if (scene.name == "PoolFinal")
+        {
+            _sources[0].loop = true;
+            _sources[0].clip = bgm;
+            _sources[0].volume = 0.7f;
+            _sources[0].Play();
+        }
     }
 
     void Start()
     {
-        _sources = GetComponents<AudioSource>().ToList();
-        _sources[0].loop = true;
-        _sources[0].clip = bgm;
-        _sources[0].volume = 0.7f;
-        _sources[0].Play();
-
         _sources[1].loop = true;
         _sources[1].clip = ambient;
         _sources[1].volume = 0.7f;
